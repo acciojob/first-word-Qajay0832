0862a2083507b1455805316ed925af0c6da9305a
Qajay0832 <123375564+Qajay0832@users.noreply.github.com>
2024-05-29 20:33:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function firstWord(s) {
+	s=s.trim();
 	const index=s.indexOf(" ")
   return index===-1?s:s.slice(0,index)
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
